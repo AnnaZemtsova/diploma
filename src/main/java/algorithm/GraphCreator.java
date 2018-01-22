24d097fb3113cf38f309d77d287d7af20bd0ab57
AnnaZemtsova <zemtsova18anna@gmail.com>
2018-01-22 19:06:13 +0200

    Update GraphCreator.java

--- src/main/java/algorithm/GraphCreator.java
+++ src/main/java/algorithm/GraphCreator.java
@@ -5,7 +5,7 @@ import data.Vehicle;
 import java.util.ArrayList;
 
  public interface GraphCreator {
-    public double[][] createGraphByMoney(ArrayList<Vehicle> vehicles, ArrayList<City> wantedCities);
-    public double [][] createGraphByTime(ArrayList<Vehicle> vehicles,ArrayList<City> wantedCities);
-    public double[][] createGraphByTimeMoney(ArrayList<Vehicle> vehicles,ArrayList<City> wantedCities,double m,double t);
+    public double[][] createGraphByMoney(ArrayList<City> wantedCities);
+    public double [][] createGraphByTime(ArrayList<City> wantedCities);
+    public double[][] createGraphByTimeMoney(ArrayList<City> wantedCities,double m,double t);
 }
