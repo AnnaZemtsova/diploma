c3e456c0994a301e91198c947890d27f75b1db2e
AnnaZemtsova <zemtsova18anna@gmail.com>
2018-01-22 19:05:58 +0200

    Update Graph.java

--- src/main/java/algorithm/Graph.java
+++ src/main/java/algorithm/Graph.java
@@ -9,25 +9,30 @@ public class Graph implements GraphCreator {
 
     private static final double INFINITY =4147483647.0;
     private static final int from =1;
+    private ArrayList<Vehicle> vehicles;
 
 
 
     //_____________________________________________________________________________________
     @Override
-    public double[][] createGraphByMoney(ArrayList<Vehicle> Vehicles,ArrayList<City> wantedCities) {
-        return createGraph( Vehicles,wantedCities,true,false ,0,0);
+    public double[][] createGraphByMoney(ArrayList<City> wantedCities) {
+        return createGraph(wantedCities,true,false ,0,0);
     }
 
     //_____________________________________________________________________________________
     @Override
-    public double[][] createGraphByTime(ArrayList<Vehicle> Vehicles,ArrayList<City> wantedCities) {
-        return createGraph(Vehicles, wantedCities,false,true ,0,0);
+    public double[][] createGraphByTime(ArrayList<City> wantedCities) {
+        return createGraph(wantedCities,false,true ,0,0);
     }
 
     //_____________________________________________________________________________________
     @Override
-    public double[][] createGraphByTimeMoney(ArrayList<Vehicle> Vehicles,ArrayList<City> wantedCities, double m, double t) {
-        return createGraph( Vehicles,wantedCities,false,false ,m,t);
+    public double[][] createGraphByTimeMoney(ArrayList<City> wantedCities, double m, double t) {
+        return createGraph(wantedCities,false,false ,m,t);
+    }
+
+    public Graph(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
     }
 
     //_____________________________________________________________________________________
@@ -35,7 +40,7 @@ public class Graph implements GraphCreator {
     из одного города в другой
     всем невозможным путям присваиваем расстояние(цену) = бесконечности
      */
-    private double[][] createGraph(ArrayList<Vehicle> Vehicles,ArrayList<City> wantedCities,boolean isMoney,
+    private double[][] createGraph(ArrayList<City> wantedCities,boolean isMoney,
                                    boolean isTime,double m,double t){
         double[][] graph = new double[wantedCities.size()+1][wantedCities.size()+1];
         for(int i=0;i<wantedCities.size()+1;i++){
@@ -48,7 +53,7 @@ public class Graph implements GraphCreator {
             graph[i][0]=i;
             graph[0][i]=i;
         }
-        for (Vehicle Vehicle : Vehicles) {
+        for (Vehicle Vehicle : vehicles) {
             for (int j = from; j < wantedCities.size()+1; j++) {
                 if (Vehicle.getFrom().getName().equals( wantedCities.get( j-1).getName() )) {
                     for (int k = from; k < wantedCities.size()+1; k++) {
@@ -56,7 +61,7 @@ public class Graph implements GraphCreator {
                             if (Vehicle.getTo().getName().equals( wantedCities.get( k-1).getName() )) {
                                if(isMoney) graph[j][k] = Vehicle.getPrice();
                                else if(isTime)  graph[j][k] = getTimeInWay( Vehicle.getDateTo(),Vehicle.getDatefrom());
-                               else graph[j][k] = m*Vehicle.getPrice()+t*getTimeInWay( Vehicle.getDatefrom(),Vehicle.getDateTo() );
+                               else graph[j][k] = m*Vehicle.getPrice()+t*getTimeInWay( Vehicle.getDateTo(),Vehicle.getDatefrom() );
                             }
                         }
                     }
